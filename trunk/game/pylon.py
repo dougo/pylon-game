c18302978a56a99b5924ba27e5388acdb0867d42
DougOrleans <DougOrleans@55b0ee54-1c48-0410-bf92-a11f6cf122dc>
2008-03-10 22:07:59 +0000

    Initial revision.

---
+++ trunk/game/pylon.py
@@ -0,0 +1,197 @@
+import volity.game
+import itertools
+import random
+
+class Game(volity.game.Game):
+
+    gamename = "Pylon"
+    gamedescription = "Pylon: an abstract strategy game" \
+                      " of placement and stacking"
+    ruleseturi = "http://steak.place.org/games/pylon/ruleset.html"
+    websiteurl = "http://www.icehousegames.org/wiki/index.php?title=Pylon"
+
+    def __init__(self, ref):
+        volity.game.Game.__init__(self, ref)
+        self.setopset(self)
+        volity.game.Seat(self, "seat1")
+        volity.game.Seat(self, "seat2")
+        self.validatecalls("place", afoot=True, seated=True, args=[int, list])
+        self.validatecalls("stack", afoot=True, seated=True, args=[list, list])
+
+    def begingame(self):
+        # TO DO: these could be configurable
+        self.rows = 5
+        self.cols = 6
+        self.pyramidSizes = 3
+        self.stashSize = 5
+        self.history = []
+        self.turnSequence = itertools.cycle(self.getseatlist())
+        self.curSeat = self.turnSequence.next()
+        self.board = {}
+        for seat in self.getseatlist():
+            seat.stash = [self.stashSize] * self.pyramidSizes
+        self.sendtable("start", self.curSeat)
+        # TO DO: only do random placement if configured
+        # self.doRandomSetup()
+
+    def rpc_place(self, sender, size, pos):
+        seat = self.getplayerseat(sender)
+        if seat != self.curSeat:
+            raise volity.game.FailureToken("notYourTurn")
+        if seat.stash[size] == 0:
+            raise volity.game.FailureToken("emptyStash")
+        pos = tuple(pos)
+        if not self.isLegalPosition(pos):
+            raise volity.game.FailureToken("illegalPosition")
+        if self.board.has_key(pos):
+            raise volity.game.FailureToken("occupiedPosition")
+        self.place(size, pos)
+
+    def rpc_stack(self, sender, fromPos, toPos):
+        seat = self.getplayerseat(sender)
+        if seat != self.curSeat:
+            raise volity.game.FailureToken("notYourTurn")
+        if any(num > 0 for num in seat.stash):
+            raise volity.game.FailureToken("placementPhase")
+        fromPos = tuple(fromPos)
+        toPos = tuple(toPos)
+        if not self.isLegalPosition(fromPos) or not self.isLegalPosition(toPos):
+            raise volity.game.FailureToken("illegalPosition")
+        if not areAdjacentPositions(fromPos, toPos):
+            raise volity.game.FailureToken("notAdjacentPositions")
+        if not self.board.has_key(fromPos) or not self.board.has_key(toPos):
+            raise volity.game.FailureToken("notOccupiedPosition")
+        top = self.board[fromPos]
+        bottom = self.board[toPos]
+        if not self.canStack(top, bottom):
+            raise volity.game.FailureToken("invalidStack")
+        self.stack(fromPos, toPos);
+
+    def isLegalPosition(self, pos):
+        return 0 <= row(pos) < self.rows and 0 <= col(pos) < self.cols
+
+    def canStack(self, top, bottom):
+        return top[-1].size <= bottom[0].size
+    
+    def place(self, size, pos):
+        self.curSeat.stash[size] -= 1
+        self.board[pos] = [Pyramid(self.curSeat, size)]
+        self.sendtable("placed", size, pos)
+        if all(num == 0 for seat in self.getseatlist() for num in seat.stash):
+            # If the last pyramid was just placed, i.e. all stashes
+            # are empty, the current player goes again.
+            pass
+        else:
+            self.endTurn()
+
+    def stack(self, fromPos, toPos):
+        self.board[toPos] = self.board[fromPos] + self.board[toPos]
+        del self.board[fromPos]
+        self.sendtable("stacked", fromPos, toPos)
+        if self.isGameOver():
+            self.endGame()
+        else:
+            self.endTurn()
+
+    def sendtable(self, *args):
+        self.history.append(args)
+        volity.game.Game.sendtable(self, *args)
+
+    def sendgamestate(self, player, seat):
+        for args in self.history:
+            self.sendplayer(player, *args)
+
+    def endTurn(self):
+        self.curSeat = self.turnSequence.next()
+
+    def isGameOver(self):
+        return not any(self.legalStackings())
+
+    def legalStackings(self):
+        for fromPos in self.board.keys():
+            top = self.board[fromPos]
+            for toPos in getAdjacentPositions(fromPos):
+                if self.board.has_key(toPos):
+                    bottom = self.board[toPos]
+                    if self.canStack(top, bottom):
+                        yield [fromPos, toPos]
+
+    def endGame(self):
+        self.gameover(*self.sortseats(lambda seat: self.score(seat)))
+        self.curSeat = None
+
+    def score(self, seat):
+        return sum([len(stack) for stack in self.board.values()
+                    if stack[0].seat == seat])
+
+    def doRandomSetup(self):
+        for i in range(self.rows * self.cols):
+            self.placeRandomly()
+
+    def placeRandomly(self):
+        sizes = [size for size in range(self.pyramidSizes)
+                 for i in range(self.curSeat.stash[size])]
+        posns = [(row, col)
+                 for row in range(self.rows)
+                 for col in range(self.cols)
+                 if not self.board.has_key((row, col))]
+        self.place(random.choice(sizes), random.choice(posns))
+
+    def stackRandomly(self):
+        self.stack(*random.choice(list(self.legalStackings())))
+
+
+class Pyramid:
+    def __init__(self, seat, size):
+        self.seat = seat
+        self.size = size
+
+# Position-related utility functions:
+
+def row(pos):
+    return pos[0]
+
+def col(pos):
+    return pos[1]
+
+def areAdjacentPositions(pos1, pos2):
+    rowdiff = abs(row(pos1) - row(pos2))
+    coldiff = abs(col(pos1) - col(pos2))
+    return (rowdiff == 1 and coldiff == 0 or
+            rowdiff == 0 and coldiff == 1)
+
+def getAdjacentPositions(pos):
+    return [(row(pos)-1, col(pos)), (row(pos), col(pos)+1),
+            (row(pos)+1, col(pos)), (row(pos), col(pos)-1)]
+
+class RandomBot(volity.bot.Bot):
+    gameclass = Game
+    boturi = "http://steak.place.org/games/pylon/random.html"
+    ruleseturi = Game.ruleseturi
+    rulesetversion = Game.rulesetversion
+
+    def __init__(self, actor):
+        volity.bot.Bot.__init__(self, actor)
+        self.setopset(self)
+        self.game = actor.referee.game
+
+    def rpc_start(self, sender, seat):
+        self.maybeMove()
+
+    def rpc_placed(self, sender, size, pos):
+        self.maybeMove()
+
+    def rpc_stacked(self, sender, fromPos, toPos):
+        self.maybeMove()
+
+    def maybeMove(self):
+        myseat = self.game.getownseat()
+        curSeat = self.game.curSeat
+        if myseat and curseat and myseat.id == curSeat.id:
+            self.move()
+
+    def move(self):
+        if any(num > 0 for num in self.game.curSeat.stash):
+            self.game.placeRandomly()
+        else:
+            self.game.stackRandomly()
---
+++ trunk/ui/main.js
@@ -0,0 +1,490 @@
+// Pylon GUI for Volity.
+
+
+// Incoming RPCs
+
+receivingState = false;
+
+volity.game_has_started = function() {
+  receivingState = true;
+};
+
+volity.state_sent = function() {
+  if (receivingState) {
+    receivingState = false;
+    displayState();
+  }
+};
+
+suspended = false;
+
+volity.suspend_game = function() {
+  suspended = true;
+  updateMessage();
+};
+
+volity.resume_game = function() {
+  suspended = false;
+  displayState();
+};
+
+volity.end_game = function() {
+  setWinners();
+  whoseTurn = null;
+  if (!receivingState)
+    displayState();
+};
+
+
+
+game.start = function(seat) {
+  initState();
+  whoseTurn = seat;
+  if (!receivingState)
+    displayState();
+};
+
+game.placed = function(size, pos) {
+  var stack = popStack(whoseTurn, size);
+  putStack(pos, stack);
+  scores[whoseTurn]++;
+  var pyramids = 0;
+  for (var seat in stashes)
+    for (var size = 0; size < sizes; size++)
+      pyramids += getPyramids(seat, size);
+  // If all stashes are empty, the current player goes again to start
+  // the stacking phase, i.e. only call nextTurn if at least one stash
+  // has pyramids remaining.
+  if (pyramids > 0)
+    nextTurn();
+  if (!receivingState)
+    displayState();
+};
+
+game.stacked = function(from, to) {
+  var topStack = getStack(from);
+  removeStack(from);
+  var bottomStack = getStack(to);
+  putStack(to, topStack.concat(bottomStack));
+  scores[bottomStack[0].seat] -= bottomStack.length;
+  scores[topStack[0].seat] += bottomStack.length;
+  nextTurn();
+  if (!receivingState)
+    displayState();
+};
+
+
+sizes = 3;
+
+rows = 5;
+cols = 6;
+
+board = null;
+stashes = null;
+
+whoseTurn = null;
+scores = null;
+winners = null;
+
+function initState() {
+  board = {};
+  // TO DO: generate these
+  stashes = {seat1: [5, 5, 5], seat2: [5, 5, 5]};
+  scores = {seat1: 0, seat2: 0};
+  whoseTurn = null;
+  winners = null;
+}
+
+
+function nextTurn() {
+  whoseTurn = (whoseTurn == "seat1" ? "seat2" : "seat1");
+}
+
+
+function makePosition(row, col) { return [row, col]; }
+function row(pos) { return pos[0]; }
+function col(pos) { return pos[1]; }
+function x(pos) { return col(pos); }
+function y(pos) { return row(pos); }
+
+// This is for using positions as keys in an associative array, which
+// must be strings in Javascript.  For some reason, RPC arrays don't
+// print this way (they print as "[row, col]"), so we have to form the
+// key string explicitly.
+function key(pos) { return row(pos) + "," + col(pos); }
+
+
+function getStack(pos) {
+  var cell = board[key(pos)];
+  return cell ? cell.stack : null;
+}
+
+function putStack(pos, stack) {
+  board[key(pos)] = {pos: pos, stack: stack};
+}
+
+function removeStack(pos) {
+  delete board[key(pos)];
+}
+
+function forEachStack(f) {
+  for each (var cell in board)
+    f(cell.pos, cell.stack);
+}
+
+
+// Return the number of pyramids of a particular size in a stash.
+function getPyramids(seat, size) {
+  // Seat might be an object, which needs to be explicitly converted
+  // to a string before being used as an associative array key.
+  // TO DO: is this a Gamut bug?  it "==" a string, why not print as a string?
+  return stashes[seat+""][size];
+}
+
+// Remove a pyramid from a stash and return it as a stack.
+function popStack(seat, size) {
+  stashes[seat+""][size]--;
+  return [{seat: seat, size: size}];
+}
+
+// The current phase of the game, either "placement" or "stacking".
+function getPhase() {
+  for (var size = 0; size < sizes; size++)
+    if (getPyramids(whoseTurn, size) != 0) return "placement";
+  return "stacking";
+}
+
+// Assuming "from" is a valid move source, return true iff "to" is a
+// valid move target.
+function isValidMove(from, to) {
+  if (row(to) < 0 || row(to) >= rows || col(to) < 0 || col(to) >= cols)
+    return false;
+  var bottom = getStack(to);
+  if (getPhase() == "placement")
+    return !getStack(to);
+  if (!getStack(to))
+    return false;
+  var rowdiff = Math.abs(row(from) - row(to));
+  var coldiff = Math.abs(col(from) - col(to));
+  if (!(rowdiff == 1 && coldiff == 0 ||
+	rowdiff == 0 && coldiff == 1))
+    return false;
+  var top = getStack(from);
+  return top[top.length - 1].size <= bottom[0].size;
+}
+
+
+function setWinners() {
+  winners = [];			// should be {}
+  var highScore = 0;
+  for (seat in scores)
+    if (scores[seat] > highScore) {
+      highScore = scores[seat];
+      winners = [];		// should be {}
+      winners[seat] = "win";
+    } else if (scores[seat] == highScore)
+      winners[seat] = "win";
+}
+
+
+// UI (Views)
+
+function displayState() {
+  if (whoseTurn)
+    seatmark(whoseTurn);
+  else if (winners)
+    seatmark(winners);
+  else
+    seatmark();
+  drawBoard();
+  drawStashes();
+  drawScores();
+  updateMessage();
+  clearDragObject();
+}
+
+
+svgNS = "http://www.w3.org/2000/svg";
+xlinkNS = "http://www.w3.org/1999/xlink";
+
+svg = document.rootElement;
+
+function makeGroup(parent) {
+  return parent.appendChild(document.createElementNS(svgNS, "g"));
+}
+
+function moveXY(node, x, y) {
+  node.setAttribute("transform", "translate(" + x + "," + y + ")");
+  return node;
+}
+
+function hide(node) {
+  if (node && node.parentNode)
+    node.parentNode.removeChild(node);
+}
+
+function raise(node) {
+  node.parentNode.appendChild(node);
+  if (node.parentNode != svg) raise(node.parentNode);
+}
+
+function makeSquare(size, parent) {
+  var square = document.createElementNS(svgNS, "rect");
+  square.setAttribute("style", "stroke:black; stroke-width:0.04");
+  square.setAttribute("width", size);
+  square.setAttribute("height", size);
+  return parent.appendChild(square);
+}
+
+// Draw the board:
+
+boardView = makeGroup(svg);
+for (var c = 0; c < cols; c++) {
+  for (var r = 0; r < rows; r++) {
+    var square = makeSquare(1, boardView);
+    square.setAttribute("fill-opacity", 0);
+    moveXY(square, c, r);
+  }
+}
+
+
+
+// TO DO: make this configurable
+// Note that these colors are also hardcoded in the SVG metadata.  :(
+colors = {seat1: "red", seat2: "deepskyblue"};
+
+
+function makePyramid(size, parent) {
+  var width = 0.3 + (size+1)/6;
+  var pyramid = makeSquare(width, parent);
+  moveXY(pyramid, (1-width)/2, (1-width)/2); // centered in 1x1 square
+  return pyramid;
+}
+
+textStyle =
+  "font-family: sans-serif; "+
+  "font-weight: bold; "+
+  "font-size: 0.3; "+
+  "pointer-events:none;";
+
+function makeHeightLabel(height, parent) {
+  var text = document.createElementNS(svgNS, "text");
+  text.setAttribute("style", textStyle);
+  text.setAttribute("text-anchor", "middle");
+  text.setAttribute("y", 0.2);
+  text.textContent = height;
+  moveXY(text, 0.5, 0.4);
+  return parent.appendChild(text);
+}
+
+function makeStackView(stack, parent) {
+  var group = makeGroup(parent);
+  var owner = stack[0].seat;
+  var height = stack.length;
+  var topSize = stack[0].size;
+  makePyramid(topSize, group).setAttribute("fill", colors[owner]);
+  if (height > 1) {
+    var bottomSize = stack[height-1].size;
+    if (bottomSize > topSize)
+      makePyramid(bottomSize, group).setAttribute("fill-opacity", 0);
+    makeHeightLabel(height, group);
+  }
+  group.setAttribute("onmousedown", "grabBoardStack(evt)");
+  return group;
+}
+
+
+boardStacks = null;
+
+function drawBoard() {
+  hide(boardStacks);
+  boardStacks = makeGroup(boardView);
+  forEachStack(drawStack);
+}
+
+function drawStack(pos, stack) {
+  var stackView = makeStackView(stack, boardStacks);
+  moveXY(stackView, x(pos), y(pos));
+}
+
+stashViews = null;
+
+// Stashes are displayed as sets of stacks.
+function drawStashes() {
+  hide(stashViews);
+  stashViews = makeGroup(svg);
+  var y = 0;
+  for (var seat in stashes) {
+    var group = makeGroup(stashViews);
+    var x = 0;
+    for (var size = 0; size < sizes; size++) {
+      var height = getPyramids(seat, size);
+      if (height > 0) {
+	var stack = makeGroup(group);
+	makePyramid(size, stack).setAttribute("fill", colors[seat]);
+	if (height > 1)
+	  makeHeightLabel(height, stack);
+	moveXY(stack, x, 0);
+	stack.setAttribute("onmousedown",
+			   "grabStashStack(evt, \"" +
+			   seat + "\", " + size + ")");
+      }
+      x++;
+    }
+    moveXY(group, cols, rows/2 + y*3 - 2);
+    y++;
+  }
+}
+
+scoreViews = null;
+
+function drawScores() {
+  hide(scoreViews);
+  if (whoseTurn && getPhase() == "placement") return;
+  scoreViews = makeGroup(svg);
+  var y = 0;
+  for (var seat in scores) {
+    var group = makeGroup(scoreViews);
+    var text = document.createElementNS(svgNS, "text");
+    text.setAttribute("style", textStyle);
+    text.textContent = seat + ": " + scores[seat];
+    group.appendChild(text);
+    // TO DO: show color somehow-- background rectangle maybe?
+    moveXY(group, cols + 0.2, rows/2 + y*3 - 1.5);
+    y++;
+  }
+}
+
+
+// Text message below the board:
+message = document.createElementNS(svgNS, "text");
+message.setAttribute("style", textStyle);
+message.textContent = "Pylon";
+svg.appendChild(message);
+moveXY(message, cols + 0.2, rows/2);
+
+function setMessage(msg) {
+  message.textContent = msg;
+}
+
+function updateMessage() {
+  if (suspended)
+    setMessage("Table is suspended.");
+  else if (winners)
+    setMessage("Game over.");
+  else if (whoseTurn != info.seat)
+    setMessage("Waiting for " + whoseTurn + "...");
+  else if (getPhase() == "placement")
+    setMessage("Place a pyramid.");
+  else
+    setMessage("Move a stack.");
+}
+
+
+
+// Drag & drop stuff:
+
+dragObject = null;
+dragMatrix = null;
+dragFrom = null;
+dragTo = null;
+dragSize = null;
+dragOffset = svg.createSVGPoint();
+
+function clearDragObject() {
+  hide(dragObject);
+  dragFrom = null;
+  dragSize = null;
+}
+
+// Return the mouse location of the event ev in the viewport
+// coordinate space.
+function getPoint(ev) {
+  var pt = svg.createSVGPoint();
+  pt.x = ev.clientX;
+  pt.y = ev.clientY;
+  return pt;
+}
+
+// Return the mouse location of the event ev in the boardView
+// coordinate space.
+function getDragPoint(ev) {
+  return getPoint(ev).matrixTransform(dragMatrix);
+}
+
+// Return the mouse location of the event ev in the event target's
+// coordinate space.
+function getDragOffset(ev) {
+  return getPoint(ev).matrixTransform(ev.currentTarget.getCTM().inverse());
+}
+
+// Return the board position containing the boardView coordinate pt.
+function getPosition(pt) {
+  return makePosition(Math.floor(pt.y), Math.floor(pt.x));
+}
+
+function grabStack(ev) {
+  raise(dragObject);
+  // Record the CTM once, assuming that the board can't be zoomed or
+  // panned while dragging!
+  dragMatrix = boardView.getCTM().inverse();
+  // Adjust from the mouse point to the upper left corner of the stack.
+  dragOffset = getDragOffset(ev);
+  svg.setAttribute("onmousemove", "dragStack(evt)");
+  svg.setAttribute("onmouseup", "dropStack(evt)");
+}
+
+function grabBoardStack(ev) {
+  if (info.seat != whoseTurn || getPhase() != "stacking") return;
+  dragObject = ev.currentTarget;
+  grabStack(ev);
+  var pt = getDragPoint(ev);
+  dragFrom = getPosition(pt);
+}
+
+function grabStashStack(ev, seat, size) {
+  if (seat != info.seat ||
+      info.seat != whoseTurn ||
+      getPhase() != "placement" ||
+      getPyramids(seat, size) == 0)
+    return;
+  dragObject = makeStackView([{seat: seat, size: size}], boardView);
+  grabStack(ev);
+  dragStack(ev); // move it to the same location as the stash stack
+  dragSize = size;
+}
+
+function dragStack(ev) {
+  var pt = getDragPoint(ev);
+  moveXY(dragObject, pt.x - dragOffset.x, pt.y - dragOffset.y);
+}
+
+function dropStack(ev) {
+  svg.removeAttribute("onmousemove");
+  svg.removeAttribute("onmouseup");
+  var pt = getDragPoint(ev);
+  var pos = getPosition(pt);
+  if (isValidMove(dragFrom, pos)) {
+    moveXY(dragObject, x(pos), y(pos));
+    dragTo = pos;
+    sendMove();
+  } else {
+    if (dragFrom)
+      // Put it back where it started.
+      moveXY(dragObject, x(dragFrom), y(dragFrom));
+    else
+      hide(dragObject);
+    dragFrom = null;
+  }
+}
+
+function sendMove() {
+  // This handles a possible race condition if the user grabs a stack
+  // before the previous move has been registered:
+  // TO DO: can this happen?  better way to prevent this?
+  // if (whoseTurn != info.seat) return;
+  if (dragFrom)
+    rpc("stack", dragFrom, dragTo);
+  else
+    rpc("place", dragSize, dragTo);
+}
